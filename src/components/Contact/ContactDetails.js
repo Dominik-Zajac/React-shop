084fd5d790141c320b968bcb1c013de464821381
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-04 09:23:40 +0200

    Edit section contact

--- src/components/Contact/ContactDetails.js
+++ src/components/Contact/ContactDetails.js
@@ -4,7 +4,7 @@ const ContactDetails = () => {
     return (
         <div className='contact-details col-sm-12 col-lg-6'>
             <h1 className='contact-title'>Kontakt</h1>
-            <hr className='line'/>
+            <hr className='line' />
             <p className='contact-paragraph'>11-111 Krakow</p>
             <p className='contact-paragraph'>ul.Krakowska 1</p>
             <p className='contact-paragraph'>tel.-111-111</p>
--- src/components/Contact/Localization.js
+++ src/components/Contact/Localization.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Localization = () => {
     return (
         <div className='localization col-sm-12 col-lg-6'>
-            <img src='https://i.ibb.co/CPsBMxC/mapa.jpg' alt='Mapa'/>
+            <img src='https://i.ibb.co/CPsBMxC/mapa.jpg' alt='Mapa' />
         </div>
     )
 }
