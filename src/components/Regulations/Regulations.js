85b033d6883ed91febcfdd03b2184a9eacf81f2c
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-04 09:34:05 +0200

    Edit section Regulations

--- src/components/Regulations/Regulations.js
+++ src/components/Regulations/Regulations.js
@@ -18,65 +18,65 @@ export const Regulations = () => {
                 <h1 className='title'>Regulamin Narzedzia</h1>
                 <h3 className='subtitle'>I. Postanowienia ogolne</h3>
                 <p className='paragraph'>
-                    1.Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    1.Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    2. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    2. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    3. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    3. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    4. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    4. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <h3 className='subtitle'>II. Transakcje</h3>
                 <p className='paragraph'>
-                    1. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    1. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    2. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    2. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    3. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    3. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    4. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    4. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <h3 className='subtitle'>III. Postanowienia koncowe</h3>
                 <p className='paragraph'>
-                    1. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    1. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    6. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    6. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    2. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    2. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
                 <p className='paragraph'>
-                    3. Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Sed tincidunt eleifend purus, 
+                    3. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Sed tincidunt eleifend purus,
                     id aliquam mi sollicitudin ut. Pellentesque ut urna urna.
                 </p>
             </div>
