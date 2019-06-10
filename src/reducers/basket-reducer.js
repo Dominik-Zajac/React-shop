e88481f23edf02044063e02c710d4ff100246536
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-10 15:36:47 +0200

    Edit reducer

--- src/reducers/basket-reducer.js
+++ src/reducers/basket-reducer.js
@@ -1,8 +1,8 @@
-import { 
-  	ADD_TO_BASKET, 
-  	REMOVE_PRODUCT, 
-  	PIECE_ADD, 
-  	PIECE_REMOVE 
+import {
+    ADD_TO_BASKET,
+    REMOVE_PRODUCT,
+    PIECE_ADD,
+    PIECE_REMOVE
 } from '../actions/actions-basket';
 import productsData from '../data/products.json';
 
@@ -18,12 +18,12 @@ const basketReducer = function (state = initialState, action) {
         case ADD_TO_BASKET:
             const basket = state.products.find(product => product.id === action.id);
             const carts = [...state.carts, basket];
-            
-            if(state.carts.find(cart => cart.id === action.id)) {
+
+            if (state.carts.find(cart => cart.id === action.id)) {
                 const addToBasketProduct = state.products.find(product => product.id === action.id);
                 const counterAddToBasket = ++addToBasketProduct.count;
                 const productPrice = parseFloat(addToBasketProduct.price);
-                
+
                 return Object.assign({}, state, {
                     pieceCounter: ++state.pieceCounter,
                     sumPrice: state.sumPrice + productPrice
@@ -33,46 +33,46 @@ const basketReducer = function (state = initialState, action) {
                 const cartPrice = parseFloat(addToBasketCart.price);
 
                 return Object.assign({}, state, {
-                    carts, 
-                    sumPrice: state.sumPrice + cartPrice, 
+                    carts,
+                    sumPrice: state.sumPrice + cartPrice,
                     pieceCounter: ++state.pieceCounter
                 });
             }
 
-        case REMOVE_PRODUCT:
-            const removeCart = state.carts.filter(product => product.id !== action.id);
-            const removeCartProduct = state.products.find(product => product.id === action.id);
-            const counterRemoveProduct = parseFloat(removeCartProduct.count);
-            const priceRemoveProduct = parseFloat(removeCartProduct.price) * counterRemoveProduct;
-            
-            return Object.assign({}, state, {
-                carts: removeCart,
-                sumPrice: state.sumPrice - priceRemoveProduct,
-                pieceCounter: state.pieceCounter - counterRemoveProduct,
-            });
+            case REMOVE_PRODUCT:
+                const removeCart = state.carts.filter(product => product.id !== action.id);
+                const removeCartProduct = state.products.find(product => product.id === action.id);
+                const counterRemoveProduct = parseFloat(removeCartProduct.count);
+                const priceRemoveProduct = parseFloat(removeCartProduct.price) * counterRemoveProduct;
 
-        case PIECE_ADD:
-            const pieceAdd = state.products.find(product => product.id === action.id);
-            const counterPieceAdd = ++pieceAdd.count; 
-            const pricePieceAdd = parseFloat(pieceAdd.price);
+                return Object.assign({}, state, {
+                    carts: removeCart,
+                    sumPrice: state.sumPrice - priceRemoveProduct,
+                    pieceCounter: state.pieceCounter - counterRemoveProduct,
+                });
 
-            return Object.assign({}, state, {
-                pieceCounter: ++state.pieceCounter, 
-                sumPrice: state.sumPrice + pricePieceAdd
-            });
+            case PIECE_ADD:
+                const pieceAdd = state.products.find(product => product.id === action.id);
+                const counterPieceAdd = ++pieceAdd.count;
+                const pricePieceAdd = parseFloat(pieceAdd.price);
 
-        case PIECE_REMOVE:
-            const pieceRemove = state.products.find(product => product.id === action.id);
-            const counterPieceRemove = --pieceRemove.count;
-            const pricePieceRemove = parseFloat(pieceRemove.price);
-            
-            return Object.assign({}, state, {
-                pieceCounter: --state.pieceCounter, 
-                sumPrice: state.sumPrice - pricePieceRemove
-            });
+                return Object.assign({}, state, {
+                    pieceCounter: ++state.pieceCounter,
+                    sumPrice: state.sumPrice + pricePieceAdd
+                });
+
+            case PIECE_REMOVE:
+                const pieceRemove = state.products.find(product => product.id === action.id);
+                const counterPieceRemove = --pieceRemove.count;
+                const pricePieceRemove = parseFloat(pieceRemove.price);
+
+                return Object.assign({}, state, {
+                    pieceCounter: --state.pieceCounter,
+                    sumPrice: state.sumPrice - pricePieceRemove
+                });
     }
 
     return state;
 };
 
-export default basketReducer;            
+export default basketReducer;
--- src/reducers/products-reducer.js
+++ src/reducers/products-reducer.js
@@ -1,8 +1,8 @@
-import { 
-    GET_PRODUCTS, 
-    GET_PRODUCT, 
-    SEARCH_PRODUCTS, 
-    SORT_BY_NAME, 
+import {
+    GET_PRODUCTS,
+    GET_PRODUCT,
+    SEARCH_PRODUCTS,
+    SORT_BY_NAME,
     SORT_BY_PRICE
 } from '../actions/actions-products';
 import productsData from '../data/products.json';
@@ -20,18 +20,18 @@ const initialState = {
 const productsReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_PRODUCTS:
-            return Object.assign({}, state, {products: state.products});
-                
+            return Object.assign({}, state, { products: state.products });
+
         case GET_PRODUCT:
             const selectedProduct = state.products.find(product => product.id == action.id);
             return Object.assign({}, state, { selectedProduct });
-            
+
         case SEARCH_PRODUCTS:
-            const foundProducts = state.products.filter(product => action.searchText.length > 1 
-            ? product.search.toLowerCase().includes(action.searchText.toLowerCase()) : state);
+            const foundProducts = state.products.filter(product => action.searchText.length > 1
+                ? product.search.toLowerCase().includes(action.searchText.toLowerCase()) : state);
             return Object.assign({}, state, { visibleProducts: foundProducts });
-            
-        case SORT_BY_NAME: 
+
+        case SORT_BY_NAME:
             const sortName = state.products.sort((a, b) => {
                 if (action.direct === "asc") {
                     let x = a[action.key];
@@ -43,19 +43,19 @@ const productsReducer = function (state = initialState, action) {
                     return x < y ? -1 : x > y ? 1 : 0;
                 }
             });
-            return Object.assign({}, state, { 
+            return Object.assign({}, state, {
                 visibleProducts: sortName,
                 direction: {
                     [action.key]: action.direct,
                 }
             });
 
-        case SORT_BY_PRICE: 
+        case SORT_BY_PRICE:
             const sortPrice = state.products.sort((a, b) => {
                 if (action.direct === 'asc') {
                     return parseFloat(a[action.key]) - parseFloat(b[action.key]);
                 } else {
-                    return parseFloat(b[action.key]) - parseFloat(a[action.key]);  
+                    return parseFloat(b[action.key]) - parseFloat(a[action.key]);
                 }
             });
             return Object.assign({}, state, {
@@ -63,7 +63,7 @@ const productsReducer = function (state = initialState, action) {
                 direction: {
                     [action.key]: action.direct,
                 }
-            });   
+            });
     }
 
     return state;
